5783aade0f42bb8239102218d7fd166cf64088af
Guilherme <guilhermegpereira26@hotmail.com>
2025-06-09 16:37:06 -0300

    Add useFetchMovies para isolar a responsabilidade de requisicao

--- src/components/MovieSection/index.tsx
+++ src/components/MovieSection/index.tsx
@@ -4,27 +4,10 @@ import InputText from '../InputText'
 import styles from './MovieSection.module.css'
 import Button from '../Button'
 import MovieList from '../MovieList'
-import { getMovies } from '../../api'
-import type { Movie } from '../../types'
-import { useEffect, useState } from 'react'
+import useFetchMovies from '../../hooks/useFetchMovies'
 
 const MovieSection = () => {
-
-  const [movies, setMovies] = useState<Movie[]>([])
-
-  const fetchMovies = async () => {
-    try {
-      const movies = await getMovies();
-
-      setMovies(movies);
-    } catch (erro) {
-      console.log("Não foi possível buscar os dados da API.")
-    }
-  }
-
-  useEffect(() => {
-    fetchMovies();
-  }, [])
+  const {movies, error, isLoading} = useFetchMovies();
 
   return (
     <main>
